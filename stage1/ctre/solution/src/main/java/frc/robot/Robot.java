8365178f0b61bbc365bab7b1015665e91fba4068
Dylan Barva <d.barva5402@gmail.com>
2026-05-15 21:18:03 -0700

    add starter code

---
+++ stage1/ctre/solution/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.simulation.DrivetrainSim;
+import frc.robot.simulation.SingleFlywheelSim;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+    private final XboxController joystick = new XboxController(0);
+
+    private final TalonFX leftFX = new TalonFX(1);
+    private final TalonFX rightFX = new TalonFX(2);
+    private final TalonFX intake = new TalonFX(3);
+    private final TalonFX shooter = new TalonFX(4);
+    private final Pigeon2 imu = new Pigeon2(0);
+    
+    private final DifferentialDrive drivetrain = new DifferentialDrive(leftFX::set, rightFX::set);
+
+    private DrivetrainSim sim = new DrivetrainSim(leftFX, rightFX, imu);
+    private SingleFlywheelSim intakeSim = new SingleFlywheelSim(intake);
+    private SingleFlywheelSim shooterSim = new SingleFlywheelSim(shooter);
+
+    private double autoStart;
+    
+
+    public Robot() {
+        TalonFXConfiguration fxCfg = new TalonFXConfiguration();
+        fxCfg.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        leftFX.getConfigurator().apply(fxCfg);
+
+        fxCfg.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        rightFX.getConfigurator().apply(fxCfg);
+
+        Pigeon2Configuration imuCfg = new Pigeon2Configuration();
+        imu.getConfigurator().apply(imuCfg);
+    }
+
+    @Override
+    public void autonomousInit() {
+        autoStart = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        if (Timer.getFPGATimestamp() - autoStart < 3.0) {
+            drivetrain.arcadeDrive(0.5, 0.0);
+        } else {
+            drivetrain.arcadeDrive(0.0, 0.0);
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        drivetrain.arcadeDrive(-joystick.getLeftY(), -joystick.getRightX());
+        if (joystick.getLeftBumperButton()) {
+            intake.set(0.5);
+        } else {
+            intake.set(0.0);
+        }
+
+        if (joystick.getRightBumperButton()) {
+            shooter.set(0.5);
+        } else {
+            shooter.set(0.0);
+        }
+    }
+
+    @Override
+    public void simulationInit() {
+      sim.init();
+      intakeSim.init();
+      shooterSim.init();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+       sim.periodic();
+       intakeSim.periodic();
+       shooterSim.periodic();
+    }
+}
