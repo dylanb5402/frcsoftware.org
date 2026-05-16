eb8b32b4c83cc852afa58c40806d67d2e1630926
Dylan Barva <d.barva5402@gmail.com>
2026-05-15 21:04:46 -0700

    Write up shooter + intake buttons

--- stage1/ctre/starterCode/src/main/java/frc/robot/simulation/SingleFlywheelSim.java
+++ stage1/ctre/starterCode/src/main/java/frc/robot/simulation/SingleFlywheelSim.java
@@ -10,6 +10,8 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 
@@ -21,6 +23,11 @@ public class SingleFlywheelSim {
     private final FlywheelSim m_flywheelSim;
     private double angularPositionRad = 0.0;
 
+    private final DoublePublisher motorVoltagePub;
+    private final DoublePublisher rotorVelocityPub;
+    private final DoublePublisher currentPub;
+    private final DoublePublisher rotorPositionPub;
+
     public SingleFlywheelSim(TalonFX motor) {
         this(motor, DCMotor.getKrakenX60Foc(1), 1.0, 0.001);
     }
@@ -31,6 +38,13 @@ public class SingleFlywheelSim {
         this.m_flywheelSim = new FlywheelSim(
                 LinearSystemId.createFlywheelSystem(gearbox, moi, gearing),
                 gearbox);
+
+        var table = NetworkTableInstance.getDefault()
+                .getTable("FlywheelSim/Motor" + motor.getDeviceID());
+        this.motorVoltagePub = table.getDoubleTopic("MotorVoltage").publish();
+        this.rotorVelocityPub = table.getDoubleTopic("RotorVelocity").publish();
+        this.currentPub = table.getDoubleTopic("Current").publish();
+        this.rotorPositionPub = table.getDoubleTopic("RotorPosition").publish();
     }
 
     public void init() {
@@ -48,5 +62,10 @@ public class SingleFlywheelSim {
 
         motorSim.setRawRotorPosition(Radians.of(angularPositionRad));
         motorSim.setRotorVelocity(RadiansPerSecond.of(velocityRadPerSec));
+
+        motorVoltagePub.set(motorSim.getMotorVoltage());
+        rotorVelocityPub.set(velocityRadPerSec);
+        currentPub.set(m_flywheelSim.getCurrentDrawAmps());
+        rotorPositionPub.set(angularPositionRad);
     }
 }
