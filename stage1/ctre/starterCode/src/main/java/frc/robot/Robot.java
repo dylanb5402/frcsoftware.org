b6878d022f8f0c3df8227aca5490aeda2ce0e5a9
Dylan Barva <d.barva5402@gmail.com>
2026-05-15 21:36:12 -0700

    Update starter code

--- stage1/ctre/starterCode/src/main/java/frc/robot/Robot.java
+++ stage1/ctre/starterCode/src/main/java/frc/robot/Robot.java
@@ -7,48 +7,39 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
 
-    // private DrivetrainSim sim = new DrivetrainSim(leftFX, rightFX, imu);
-    // private SingleFlywheelSim intakeSim = new SingleFlywheelSim(intake);
-    // private SingleFlywheelSim shooterSim = new SingleFlywheelSim(shooter);
+  // private DrivetrainSim sim = new DrivetrainSim(leftFX, rightFX, imu);
+  // private SingleFlywheelSim intakeSim = new SingleFlywheelSim(intake, "Intake");
+  // private SingleFlywheelSim shooterSim = new SingleFlywheelSim(shooter, "Shooter");
 
-    public Robot() {
+  public Robot() {}
 
-    }
+  @Override
+  public void autonomousInit() {}
 
-    @Override
-    public void autonomousInit() {
+  @Override
+  public void autonomousPeriodic() {}
 
-    }
+  @Override
+  public void teleopPeriodic() {}
 
-    @Override
-    public void autonomousPeriodic() {
+  @Override
+  public void simulationInit() {
+    // sim.init();
+    // intakeSim.init();
+    // shooterSim.init();
+  }
 
-    }
-
-    @Override
-    public void teleopPeriodic() {
-
-    }
-
-    @Override
-    public void simulationInit() {
-    //   sim.init();
-    //   intakeSim.init();
-    //   shooterSim.init();
-    }
-
-    @Override
-    public void simulationPeriodic() {
-    //    sim.periodic();
-    //    intakeSim.periodic();
-    //    shooterSim.periodic();
-    }
+  @Override
+  public void simulationPeriodic() {
+    // sim.periodic();
+    // intakeSim.periodic();
+    // shooterSim.periodic();
+  }
 }
